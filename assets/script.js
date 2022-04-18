f47a5f2f72df4952d94920155601547f3a9b829e
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-04-17 18:10:49 -0700

    set countdown timer

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,42 @@ var submitBtn = document.getElementById("submit");
 var intitialsEl = document.getElementById("initials");
 var choicesEl = document.getElementById("choices");
 
+var questionIndex = 0;
+var time = 90;
+var timer;
+
+
+// Start Quiz
+function startQuiz() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    // make questions visible
+    questionsEl.removeAttribute("class");
+
+    //start countdown
+    timer = setInterval()
+
+}
+
+// Timer
+function countdown() {
+    time--;
+    timer.textContent = time;
+}
+
+
+// Questions
+
+
+//Ending the quiz
+
+
+// Saving results
+
+
+
+
+
 
 
 
