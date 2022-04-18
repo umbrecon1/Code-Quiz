a672dcafd5e1134ca375165084abaf581ed28c4a
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-04-17 17:36:52 -0700

    added variables to js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 
-}
+// Variables
+
+var questionsEl = document.getElementById("questions");
+var timeEl = document.getElementById("time");
+var startBtn = document.getElementById("start");
+var submitBtn = document.getElementById("submit");
+var intitialsEl = document.getElementById("initials");
+var choicesEl = document.getElementById("choices");
+
+
 
 
 
